bc812469fc31f9cb1dd3accf88f3ee257b133bea
jer <alphastrata@gmail.com>
2025-02-02 06:59:22 +1100

    smeh

--- crates/poe_tree/examples/node_count.rs
+++ crates/poe_tree/examples/node_count.rs
@@ -6,11 +6,16 @@ fn main() {
     let tree_data: serde_json::Value = serde_json::from_reader(reader).unwrap();
     let mut tree = PassiveTree::from_value(&tree_data).unwrap();
     println!("Total number of nodes [ALL] : {}", tree.nodes.len());
+    println!("Total number of edges [ALL]: {}", tree.edges.len());
 
     tree.remove_hidden(); // This prunes everything NOT in the default passives, so no ascendencies etc.
     println!(
         "Total number of nodes [JUST THE VANILLA PASSIVES]: {}",
         tree.nodes.len()
+    );
+    println!(
+        "Total number of edges [JUST THE VANILLA PASSIVES]: {}",
+        tree.edges.len()
     );
     /*
     Total number of nodes [ALL] : 3557
